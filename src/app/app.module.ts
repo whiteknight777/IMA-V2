4dd9cffa7409c5b13e29077d41eef45e3b7289dd
ARRA Désiré <desirearra@gmail.com>
2019-04-05 02:11:15 +0200

    Création des routes et des composants : Login, Home, Header, Notfound

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './_helpers/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    NotfoundComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
